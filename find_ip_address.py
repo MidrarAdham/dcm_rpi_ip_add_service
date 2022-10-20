86baa74eabd08fcae25814aebd440518f7d98c36
sonalinf <narmada@pdx.edu>
2022-10-20 14:58:41 -0700

    commented the script

--- find_ip_address.py
+++ find_ip_address.py
@@ -2,15 +2,14 @@ import os
 import time
 import paramiko
 
-print('Hey, welcome!')
-os.system('hostname -I >> ip_.txt')
+print('Hey, welcome!')# Welcome message. Not important. I was bored.
+os.system('hostname -I >> ip_.txt') # os command to get ip address
 ssh = paramiko.SSHClient() 
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('10.218.219.90', username='deras', password='powerlab1111')
+ssh.connect('10.218.219.90', username='deras', password='powerlab1111') # fill in credentials for the remote host.
 sftp = ssh.open_sftp()
-# Localpath is a string containing the path of the local file, remotepath is where the file should be copied on the raspberry pi
-local_file = 'ip_.txt'
-remote_host = '/home/deras/Desktop/'
+local_file = 'ip_.txt' # name of the file you want to send over to the remote host.
+remote_host = '/home/deras/Desktop/trial.txt' #Directory is not enough. Need to specify a file name!
 sftp.put(local_file, remote_host)
 sftp.close()
 ssh.close()
