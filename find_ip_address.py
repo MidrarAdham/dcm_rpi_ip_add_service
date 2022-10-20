10e5e27aa54c850209ea7a58e7ae56003aa58984
sonalinf <narmada@pdx.edu>
2022-10-20 14:46:43 -0700

    updated and modified script. Should add a readme

--- find_ip_address.py
+++ find_ip_address.py
@@ -5,11 +5,12 @@ import paramiko
 print('Hey, welcome!')
 os.system('hostname -I >> ip_.txt')
 ssh = paramiko.SSHClient() 
-ssh.connect('ip', username='*****', password='************')
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('10.218.219.90', username='deras', password='powerlab1111')
 sftp = ssh.open_sftp()
 # Localpath is a string containing the path of the local file, remotepath is where the file should be copied on the raspberry pi
-local_file = ''
-remote_host = ''
+local_file = 'ip_.txt'
+remote_host = '/home/deras/Desktop/'
 sftp.put(local_file, remote_host)
 sftp.close()
 ssh.close()
