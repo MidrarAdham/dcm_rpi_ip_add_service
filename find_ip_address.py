3eb3d9f938a9a0c8643c54d4343681c9761c7cf7
MidrarAdham <midrar@pdx.edu>
2022-10-20 18:01:14 -0700

    removed linux machine creds

--- find_ip_address.py
+++ find_ip_address.py
@@ -8,7 +8,7 @@ os.system('sudo hostname -I >> ip.log') # os command to get ip address
 os.system('sudo hostname >> ip.log') # os command to get the hostname
 ssh = paramiko.SSHClient() 
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('10.218.219.90', username='deras', password='powerlab1111') # fill in credentials for the remote host.
+ssh.connect('**.***.***.**', username='*****', password='***') # fill in credentials for the remote host. Ask Midrar if you don't have a host credentials.
 sftp = ssh.open_sftp()
 local_file = 'ip.log' # name of the file you want to send over to the remote host.
 remote_host = '/home/deras/Desktop/store_ip.log' #Directory is not enough. Need to specify a file name!
